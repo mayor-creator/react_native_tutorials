bde83ae434c2f42ffe5359cfea95767524a718ba
Mayor Creator <paulaboagye@outlook.com>
2025-10-20 12:00:26 -0400

    add: pan and tap gestures functions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { type ImageSourcePropType, StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
@@ -51,7 +52,7 @@ export default function Index() {
 	const onSaveImageAsync = async () => {};
 
 	return (
-		<View style={styles.container}>
+		<GestureHandlerRootView style={styles.container}>
 			<View style={styles.imageContainer}>
 				<ImageViewer
 					imgSource={PlaceholderImage}
@@ -89,7 +90,7 @@ export default function Index() {
 			<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
 				<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
 			</EmojiPicker>
-		</View>
+		</GestureHandlerRootView>
 	);
 }
 

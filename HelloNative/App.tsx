641791c99a1aade45a77cfa83b8e1def8f231934
Mayor Creator <paulaboagye@outlook.com>
2025-11-04 12:08:40 -0500

    feat: learn how to create a button using the Pressable component.

--- HelloNative/App.tsx
+++ HelloNative/App.tsx
@@ -1,17 +1,19 @@
 import { Image } from "expo-image";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<StatusBar style="auto" />
 			<Text>Hello World</Text>
+
 			<View style={{ padding: 10 }}>
 				<Text style={styles.baseText}>
 					Good morning, <Text style={styles.innerText}>how're you doing?</Text>
 				</Text>
 			</View>
+
 			<View>
 				<Image
 					style={styles.image}
@@ -20,6 +22,18 @@ export default function App() {
 					transition={1000}
 					accessibilityLabel="mountain in the cloud"
 				/>
+			</View>
+
+			<View style={styles.buttonContainer}>
+				<Pressable
+					style={({ pressed }) => [
+						styles.button,
+						pressed && styles.buttonPressed,
+					]}
+					onPress={() => alert("Button pressed")}
+				>
+					<Text style={styles.buttonText}>Press Me</Text>
+				</Pressable>
 			</View>
 		</View>
 	);
@@ -43,5 +57,29 @@ const styles = StyleSheet.create({
 		height: 200,
 		marginTop: 20,
 		borderRadius: 20,
+	},
+	buttonContainer: {
+		width: 320,
+		height: 68,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 3,
+	},
+	button: {
+		backgroundColor: "#007aff",
+		borderRadius: 20,
+		height: "100%",
+		width: "100%",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	buttonText: {
+		color: "#fff",
+		fontSize: 16,
+		fontWeight: "bold",
+	},
+	buttonPressed: {
+		backgroundColor: "#bc590eff",
+		transform: [{ scale: 0.98 }],
 	},
 });

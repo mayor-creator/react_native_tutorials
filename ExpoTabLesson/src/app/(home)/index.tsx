631e30ea7a623931cf12d3a2e03b6b672a9e3053
Mayor Creator <paulaboagye@outlook.com>
2025-11-21 12:37:31 -0500

    feat: Implement a custom modal component with state management and dedicated styles.

--- ExpoTabLesson/src/app/(home)/index.tsx
+++ ExpoTabLesson/src/app/(home)/index.tsx
@@ -1,4 +1,5 @@
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Alert, Modal, StyleSheet, Text, View } from "react-native";
 import { Button } from "@/src/components/Button";
 
 const handleOpenAlert = () => {
@@ -6,14 +7,44 @@ const handleOpenAlert = () => {
 };
 
 export default function Index() {
+	const [isModal, setIsModal] = useState(Boolean);
+
 	return (
 		<View style={styles.container}>
 			<Text>Expo Tab Lesson</Text>
 			<Button
-				title="alert"
+				title="Open Alert"
 				bgColor="#527f50"
 				onPress={handleOpenAlert}
 			></Button>
+
+			<Button
+				title="Open Modal"
+				bgColor="#527f50"
+				onPress={() => setIsModal(true)}
+			></Button>
+
+			{/*create a custom modal */}
+			<Modal
+				visible={isModal}
+				onRequestClose={() => setIsModal(false)}
+				transparent
+				//presentationStyle="pageSheet"
+				animationType="slide"
+			>
+				<View style={styles.modalContainer}>
+					<View style={styles.modalChildContainer}>
+						<Text style={styles.modalText}>A custom styled modal!</Text>
+						<Button
+							title="Close"
+							bgColor="#252f50"
+							onPress={() => {
+								setIsModal(false);
+							}}
+						></Button>
+					</View>
+				</View>
+			</Modal>
 		</View>
 	);
 }
@@ -24,5 +55,19 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "#e51c5fff",
+		gap: 10,
+	},
+	modalContainer: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	modalChildContainer: {
+		padding: 12,
+		borderRadius: 14,
+		backgroundColor: "#ed872d",
+	},
+	modalText: {
+		fontSize: 18,
 	},
 });
